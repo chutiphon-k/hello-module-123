5d31dbedf73bc31fb123771ce394687284dc7bd7
Chutiphon.k <chutiphon.k@ku.th>
2017-08-11 01:06:41 +0700

    5

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+console.log('hello eieiza');
+
+function Function1(x) {
+	return 'hello ' + x;
+}
+
+exports.Function1 = Function1;
