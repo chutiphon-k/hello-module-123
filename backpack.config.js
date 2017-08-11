f2098ec77692fc54d40ca9c60c02e5ba1eb4501c
Chutiphon.k <chutiphon.k@ku.th>
2017-08-11 12:15:22 +0700

    aaaaaaa

--- backpack.config.js
+++ backpack.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   webpack: (config, options, webpack) => {
 
+	// Object.assign(config.output, {
+	// 	publicPath: '/static/',
+	// 	filename: 'bundle.js',
+	// })
+
+	// Object.assign(config.module, {
+
+	// })
+
+	// config.module.rules.push(
+	// 	{
+	// 		test: /\.(js|jsx)$/,
+	// 		use: 'babel',
+	// 	}
+	// )
+
 	config.plugins.push(
 		new webpack.optimize.UglifyJsPlugin({
 			sourceMap: true,
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import Function2 from './akc'
-
 console.log('hello eieiza')
 
 function Function1 (x) {
-	return `hello ${x}` + Function2()
+	return 'aaaaaaa'
 }
 
 export {
